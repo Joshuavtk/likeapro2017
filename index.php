47d6f4ff1d43cec4165bcbb7cfb91f5e76979200
Joshua van 't Kruis <Joshua van 't Kruis>
2017-04-03 22:40:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+// Inserts configuration file
+include 'includes/config.php';
+
+// Inserts head
+include 'views/head.php';
+
+// Inserts header
+include 'views/header.php';
+
+// Inserts menu
+include 'views/menu.php';
+
+// Inserts homescreen
+include 'views/home.php';
+
+// Inserts footer
+include 'views/footer.php';
+?>
